e80048e59dc3f98d42e52620e48bf15f718480c5
evan-chernicky <echernicky@gmail.com>
2022-08-17 10:20:03 -0400

    added resizer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import TopHeader from './desktop/TopHeader'
 import DesktopNav from './desktop/DesktopNav'
 import MobileNav from './mobile/MobileNav'
 
-function header() {
+function Header() {
+
+  const [width, setWidth] = useState(window.innerWidth)
+
+  window.addEventListener("resize", updateDimensions)
+  function updateDimensions () {
+    setWidth(window.innerWidth)
+}       
+console.log(width)
+
   return (
-    <header className="App-header p-5">
-        <div className="container md:block hidden">
+    <header className="App-header absolute top-0 left-0 w-full">
+      {width > 768 ? (
+        <div className="container">
             <TopHeader />
             <DesktopNav />
         </div>
-        <div className="container block md:hidden">
+        ) : (
+        <div className="container">
             <MobileNav />
         </div>
+        )}
     </header>
   )
 }
 
-export default header
+export default Header
